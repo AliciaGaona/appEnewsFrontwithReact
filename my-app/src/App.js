85aa61bf772829da7f55bc6faa38f955870bc57b
AliciaGaona <aligaovela@gmail.com>
2024-06-26 01:50:40 -0600

    se agrega front con react, con funcionalidad de filtrado, usando una api test externa, ya que la que contrui sigue con el bug de que no e deja configurar cors

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import BuscarComponent from './componets/BuscarComponent';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Catalogo</h1>
+    
+    <BuscarComponent></BuscarComponent>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/componets/BuscarComponent.js
@@ -0,0 +1,74 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+
+
+const BuscarComponent = () => {
+    //configurar los hooks de usestate
+    const [users, setUsers] = useState([])
+
+    //hook de busqueda
+    const [buscar, setBuscar] = useState('') //variable de estado= buscar, funcion que la actualizara setBuscar
+
+    //funcion que trae datos de la API
+    const url = 'https://jsonplaceholder.typicode.com/users'
+
+    const showData = async () => {
+        const response = await fetch(url)
+        const data = await response.json()
+        //pasamos el data json a la variable de estado de usestate
+        setUsers(data)
+    }
+
+    //busqueda de dato, trae el valor del input buscar
+    const buscarIr = (e) => {
+        setBuscar(e.target.value)
+        console.log(e.target.value)
+    }
+
+
+    //filtrado de datos
+    let results=[] //arreglo que guardara el resultado
+    if (!buscar) {
+        results=users// si no hay nda en buscar, llena con datos del setUsers
+
+    }else{
+        //filtra datos de primer get, por el name
+        results = users.filter((dato)=>
+            dato.name.toLowerCase().includes(buscar.toLocaleLowerCase())      
+        )
+
+        //..agregar filtros por todas columnas, automatizarlo
+    }
+
+    useEffect(() => {
+        showData()
+    }, [])
+
+    //rederizar vista
+    return (
+        <div>
+            <input value={buscar} onChange={buscarIr} placeholder='Buscar' className='form-control'></input>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Name</th>
+                        <th scope="col">User Name</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        results.map((user) => (
+                            <tr key={user.id}>
+                                {/* interpolamos data con la variable de usestate*/}
+                                <td>{user.name}</td>
+                                <td>{user.username}</td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default BuscarComponent
